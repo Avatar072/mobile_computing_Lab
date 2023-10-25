3a239f0b17f30d050a596ef6cc36a0ebf6663eab
SHChen <bob100163@gmail.com>
2023-10-25 16:34:05 +0800

    first commit for kivy

---
+++ kivy_test.py
@@ -0,0 +1,13 @@
+
+import kivy
+kivy.require('2.0.0') # 注意匹配版本號
+
+from kivy.app import App
+from kivy.uix.label import Label
+
+class MyApp(App):
+    def build(self):
+        return Label(text='Hello world')
+
+if __name__ == '__main__':
+    MyApp().run()
