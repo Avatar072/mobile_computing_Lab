9251331cdb76b785b252c631ddaa6469bf5fd9ff
SHChen <bob100163@gmail.com>
2023-10-25 21:34:36 +0800

    main.py Upload file ver

--- main.py
+++ main.py
@@ -44,28 +44,40 @@ from kivy.uix.button import Button
 from kivy.uix.popup import Popup
 from kivy.uix.filechooser import FileChooserListView
 
-class ImageUploadLayout(BoxLayout):
-    file_chooser = FileChooserListView()
+from kivy.app import App
 
+class ImageUploadLayout(BoxLayout):
     def __init__(self, **kwargs):
         super(ImageUploadLayout, self).__init__(**kwargs)
         self.orientation = 'vertical'
-        upload_button = Button(text='Upload Image', size_hint_y=None, height='48dp')
-        upload_button.bind(on_release=self.select_image)
-        self.add_widget(upload_button)
-        self.image_preview = Image(source='')
-
-    def select_image(self, instance):
+        
+        # 添加返回按钮
+        self.back_button = Button(text='Back', size_hint_y=None, height='48dp')
+        self.back_button.bind(on_release=self.go_back)
+        self.add_widget(self.back_button)
+        
+        self.upload_button = Button(text='Upload Image', size_hint_y=None, height='48dp')
+        self.upload_button.bind(on_release=self.select_image)
+        self.add_widget(self.upload_button)
+        
+        self.image_preview = Image(source='', size_hint=(None, None), size=(300, 300))
+        self.add_widget(self.image_preview)  # 将 image_preview 添加到布局中
+        
         self.file_chooser = FileChooserListView()
         self.file_chooser.bind(on_submit=self.load_image)
+    
+    def select_image(self, instance):
         popup = Popup(title='Select an Image', content=self.file_chooser, size_hint=(0.9, 0.9))
         popup.open()
-
+    
     def load_image(self, instance, selection, touch):
         if len(selection) > 0:
             image_path = selection[0]
             self.image_preview.source = image_path
-            self.image_preview.id = 'image_preview'  # 设置id属性
+    
+    def go_back(self, instance):
+        # 添加返回按钮的处理逻辑
+        self.image_preview.source = ''  # 清空图像预览
 
 class ImageUploadApp(App):
     def build(self):
